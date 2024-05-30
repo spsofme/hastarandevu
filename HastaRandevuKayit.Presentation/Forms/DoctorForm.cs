88c16ccbea244442511cdee3277d4ea50c7a5994
Yunus Çiçek <yunusciccek@gmail.com>
2024-05-30 18:09:37 +0300

    front2

--- HastaRandevuKayit.Presentation/Forms/DoctorForm.cs
+++ HastaRandevuKayit.Presentation/Forms/DoctorForm.cs
@@ -17,5 +17,40 @@ namespace HastaRandevuKayit.Presentation.Forms
         {
             InitializeComponent();
         }
+
+        private void txtAddName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAddSurname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
